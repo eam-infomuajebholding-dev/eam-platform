c4756353959c7671513e2fa48a28332a2e1c4491
MGX <admin@mgx.dev>
2026-06-10 10:30:57 +0000

    Modify 1 files: app/frontend/src/App.tsx

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from '@/contexts/ThemeContext';
+import { ContentProvider } from '@/components/ContentProvider';
 import Index from './pages/Index';
 import EngineeringServices from './pages/EngineeringServices';
 import GovernmentServices from './pages/GovernmentServices';
@@ -55,10 +56,12 @@ const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
       <ThemeProvider>
-        <Toaster />
-        <BrowserRouter>
-          <AppRoutes />
-        </BrowserRouter>
+        <ContentProvider>
+          <Toaster />
+          <BrowserRouter>
+            <AppRoutes />
+          </BrowserRouter>
+        </ContentProvider>
       </ThemeProvider>
     </TooltipProvider>
   </QueryClientProvider>
